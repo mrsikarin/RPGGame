ac72b50bc35f6655c93f71813f904e7b87163c1c
mrsikarin <42531318+mrsikarin@users.noreply.github.com>
2022-07-12 16:46:20 +0700

    [mod] monster [add] boss

--- trigger_script/ScriptableNode_12.lua
+++ trigger_script/ScriptableNode_12.lua
@@ -6,12 +6,16 @@ local curExp = player:getValue("exp")
 local level = player:getValue("level")
 local maxXp = level * 100
 local status = UI:openWindow("playerUI")
+local levelup = false
 if curExp + exp >= maxXp then
   player:setValue("level",level+1)
   player:setValue("exp",(curExp + exp) - maxXp)
-   player:setValue("point",player:getValue("point") + 4)
+  player:setValue("point",player:getValue("point") + 4)
+  levelup = true
 else
   player:setValue("exp",curExp + exp)
 end
 --status:refreshBar()
 PackageHandlers.sendClientHandler("refreshBar",{},function(...) end)
+
+return levelup
